a95bcb657a6bc85675980053db281ad003213bce
Juliet <chepngetichjuliet@gmail.com>
2019-08-07 10:20:20 +0300

    creates urls for join

--- hood/urls.py
+++ hood/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     url(r'^$',views.home,name='home'),
     url(r'^profile/(\d+)$',views.profile,name='profile'),
     url(r'^update/profile/(\d+)$',views.update_profile,name='update_profile'),
+    url(r'^join/(\d+)$', views.join, name='join'),
 
 ]
