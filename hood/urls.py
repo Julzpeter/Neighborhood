2bcf2d24d9224691fffa945d2cb82b6759d7391b
Juliet <chepngetichjuliet@gmail.com>
2019-08-07 13:00:52 +0300

    adds static

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.home,name='home'),
@@ -13,3 +15,6 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^leave/$', views.leave, name='leave'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
