ac984d97acd939f18068111c0056eaf322b92fdf
Juliet <chepngetichjuliet@gmail.com>
2019-08-07 11:56:32 +0300

    adds urls for post

--- hood/urls.py
+++ hood/urls.py
@@ -9,5 +9,6 @@ urlpatterns=[
     url(r'^neighborhood/(\d+)$', views.neighborhood, name='neighborhood'),
     url(r'^business/(\d+)$', views.business, name='business'),
     url(r'^add/business/(\d+)$', views.add_business, name='add-business'),
-
+    url(r'^post/(/d+)$',views.post,name='post'),
+    
 ]
