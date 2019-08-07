f9b44c558e3baac35d6b5b822b140a9a493dae2e
Juliet <chepngetichjuliet@gmail.com>
2019-08-07 08:19:11 +0300

    adds urls for updating the profile

--- hood/urls.py
+++ hood/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.home,name='home'),
     url(r'^profile/(\d+)$',views.profile,name='profile'),
+    url(r'^update/profile/(\d+)$',views.update_profile,name='update_profile'),
 
 ]
