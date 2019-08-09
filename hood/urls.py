4405efd50df904ef3bd0d4b411ea4f2d709c8093
Juliet <chepngetichjuliet@gmail.com>
2019-08-09 12:11:30 +0300

    corrects the url pattern for post

--- hood/urls.py
+++ hood/urls.py
@@ -6,13 +6,13 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$', views.home, name='home'),
     url(r'^accounts/profile', views.home, name='home'),
-    url(r'^profile/(\d+)$',views.profile,name='profile'),
-    url(r'^update/profile/(\d+)$',views.update_profile,name='update_profile'),
+    url(r'^profile/(\d+)$', views.profile, name='profile'),
+    url(r'^update/profile/(\d+)$', views.update_profile, name='update_profile'),
     url(r'^join/(\d+)$', views.join, name='join'),
     url(r'^neighborhood/(\d+)$', views.neighborhood, name='neighborhood'),
     url(r'^business/(\d+)$', views.business, name='business'),
     url(r'^add/business/(\d+)$', views.add_business, name='add-business'),
-    url(r'^post/(/d+)$',views.post,name='post'),
+    url(r'^post/(\d+)$',views.post,name='post'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^leave/$', views.leave, name='leave'),
 ]
